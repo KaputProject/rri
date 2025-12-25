d4675d5d8fe3e6bcc0ae3ff9976816944fdace24
kuderluka <luka.kuder@gmail.com>
2025-12-25 13:16:18 +0100

    KAPUT-15: Movement kamera dodelan

--- core/src/main/java/si/um/feri/maprri/raster/config/Config.java
+++ core/src/main/java/si/um/feri/maprri/raster/config/Config.java
@@ -7,8 +7,14 @@ import si.um.feri.maprri.raster.utils.MapRasterTiles;
 public class Config {
     public static final int NUM_TILES = 3;
     public static final int ZOOM = 15;
+    public static final float INITIAL_ZOOM = 1f;
     public static final int MAP_WIDTH = MapRasterTiles.TILE_SIZE * NUM_TILES;
     public static final int MAP_HEIGHT = si.um.feri.maprri.raster.utils.MapRasterTiles.TILE_SIZE * NUM_TILES;
     public static final int HUD_WIDTH = Gdx.graphics.getWidth();
     public static final int HUD_HEIGHT = Gdx.graphics.getHeight();
+
+    public static final float PERSPECTIVE_CAMERA_INITIAL_Z = 300f;
+    public static final float PERSPECTIVE_CAMERA_NEAR = 1f;
+    public static final float PERSPECTIVE_CAMERA_FAR = 1500f;
+    public static final float CAMERA_MOVEMENT_SPEED = 3f;
 }
